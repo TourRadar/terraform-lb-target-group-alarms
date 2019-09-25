4c51b0530fd2b0ccc6af822ce60a3b4ad0630cbe
Chris Bednarczyk <chris@tourradar.com>
2019-09-25 11:46:05 +0200

    Fix output style

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,15 @@
 output "target_response_time" {
-  value = aws_cloudwatch_metric_alarm.target_response_time[0]
+  value = aws_cloudwatch_metric_alarm.target_response_time
 }
 
 output "healthy_host_count" {
-  value = aws_cloudwatch_metric_alarm.healthy_host_count[0]
+  value = aws_cloudwatch_metric_alarm.healthy_host_count
 }
 
 output "http_code_target_4xx_count" {
-  value = aws_cloudwatch_metric_alarm.http_code_target_4xx_count[0]
+  value = aws_cloudwatch_metric_alarm.http_code_target_4xx_count
 }
 
 output "http_code_target_5xx_count" {
-  value = aws_cloudwatch_metric_alarm.http_code_target_5xx_count[0]
+  value = aws_cloudwatch_metric_alarm.http_code_target_5xx_count
 }
